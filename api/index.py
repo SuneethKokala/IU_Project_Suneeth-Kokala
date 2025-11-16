81d492de4fd871161c481d1de3fdeeda880a1777
Suneethk16 <suneethk176@gmail.com>
2025-11-16 14:14:04 +0100

    Fix Vercel deployment structure

---
+++ api/index.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '''
+    <h1>Safety Monitoring System</h1>
+    <p>Dashboard deployed on Vercel</p>
+    <p>Note: Camera functionality requires local deployment</p>
+    '''
+
+if __name__ == '__main__':
+    app.run()
